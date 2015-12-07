600d2abfa04411af982e69e20d03494dd2310f40
Thomas Luquet <thomas@luquet.net>
2015-12-07 15:49:01 +0100

    Add de masse trucs dans emacs. (Email, company ...)

--- init.el
+++ init.el
@@ -1,19 +1,19 @@
 ;; Dot emacs de ouam - zobi8225
 
-;; TEST POUR UN SERVER EMACS
-(require 'server)
-(unless (server-running-p)
-  (server-start))
 
 
 
-;; PACKAGE -- Il y a peut etre trop de package list
+;; PACKAGE Source
+;; ---------------------------------------------------
+;; Il y a peut etre trop de list-package dans ma list...
+
 (require 'package)
 (setq package-archives '(
                          ("ELPA" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")
+			 ("elpy" . "https://jorgenschaefer.github.io/packages/")
 			 )
       )
 
@@ -21,24 +21,87 @@
 (setq url-http-attempt-keepalives nil)
 
 
+;; COMPANY
+;; -------------------------------------------------------
+;; Nécessaire pour avoir un popup qui propose de la completion
+
+(require 'popup)
+(require 'company)
+;; La on dit que c'est pour tout
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Company pour emoji :-)
+(require 'company-emoji)
+(add-to-list 'company-backends 'company-emoji)
+
+;; Company pour le HTML
+;; Jamais testé
+(require 'company-web-html)
+
+;; COMPANY for Python
+;; On utilise JEDI pour la completion : Attention, il demande une installation un peu bizzare quand on le lence
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+(add-hook 'python-mode-hook 'my/python-mode-hook)
 
 
+;; Don't enable company-mode in below major modes : pas dans le shell, ni erc ...
+(setq company-global-modes '(not eshell-mode comint-mode erc-mode rcirc-mode))
+
+;; "text-mode" is a major mode for editing files of text in a human language"
+;; most major modes for non-programmers inherit from text-mode
+(defun text-mode-hook-setup ()
+  ;; make `company-backends' local is critcal
+  ;; or else, you will have completion in every major mode, that's very annoying!
+  (make-local-variable 'company-backends)
+
+  ;; company-ispell is the plugin to complete words
+  (add-to-list 'company-backends 'company-ispell) )
+
+  ;; => Désactivé
+  ;; OPTIONAL, if `company-ispell-dictionary' is nil, `ispell-complete-word-dict' is used
+  ;;  but I prefer hard code the dictionary path. That's more portable.
+  ;; (setq company-ispell-dictionary (file-truename "~/.emacs.d/misc/english-words.txt")))
+
+(add-hook 'text-mode-hook 'text-mode-hook-setup)
+
+(defun toggle-company-ispell ()
+  (interactive)
+  (cond
+   ((memq 'company-ispell company-backends)
+    (setq company-backends (delete 'company-ispell company-backends))
+    (message "company-ispell disabled"))
+   (t
+    (add-to-list 'company-backends 'company-ispell)
+    (message "company-ispell enabled!"))))
+
+
+;; ---------------------------------------------------
+
+
+
+;; Org Mode
+;; ---------------------------------------------------
 ;; Test de config de Org : ne sais pas a quoi ca sert
 (require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
+;;(define-key global-map "\C-cl" 'org-store-link)
+;;(define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
 
-;; Langue dico
+
+;; Correcteur orthographique / dictinnaire
+;; ---------------------------------------------------
+(require 'flyspell)
 (add-hook 'org-mode-hook 'turn-on-flyspell) ;; Ajoute automatiquement le flysper aux fichier org
 (setq ispell-dictionary "french")
+;; ---------------------------------------------------
 
 
-;; Affiche le volume
-(require 'volume)
 
-;; Indent un rectangle
+;; Tricks
+;; ---------------------------------------------------
+;; Indentation d'une région
 (global-set-key (kbd "C-x C-a") 'indent-region)
 
 ;; Pour avoir les parenthese coloré automatiquement
@@ -59,7 +122,11 @@
 (when (fboundp 'scroll-bar-mode) ;; Enlever la scrollbar
   (scroll-bar-mode -1))
 
-;; Permet de faire des trucs mais ont sait pas quoi
+;; Affiche le volume
+(require 'volume)
+
+
+;; Permet de changer le répertoire de sauvegarde automatique
 (require 'saveplace)
 (setq-default save-place t)
 
@@ -76,6 +143,8 @@
 
 ;; Expend
 (global-set-key (kbd "M-/") 'hippie-expand)
+
+;; Buffer list
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 
@@ -88,29 +157,29 @@
 ;; Batterie dans la buffer line
 (display-battery-mode t) ;; Sert a afficher la batterie (utile pour les PC portable)
 
-;; Permet de savoir ou tu es dans ta page
-(nyan-mode t) ;; Nyan mod utile pour savoir ou on en est dans la page
+;; Nyan-mode : Permet de savoir ou tu es dans ta page (Assez utile finalement)
+(nyan-mode t)
 
 ;; Affiche l'heure dans la barre du bas
 ;; Set le buffer du de la date et du temps
 (setq display-time-24hr-format t display-time-day-and-date t display-time-interval 50 display-time-default-load-average nil display-time-mail-string "")
-
-
 (display-time-mode t) ;; affiche le temps
 
 ;; Montre les Whites space inutile en fin de ligne
 ;; TODO : show uniquement dans le code ET dans les .org
-;; (setq-default show-trailing-whitespace t)
-;;  (setq-default show-leading-whitespace t) ;; espace en fin de ligne
-;; (setq-default indicate-empty-lines t)
+(setq-default show-trailing-whitespace t)
+;; (setq-default show-leading-whitespace t) ;; espace en fin de ligne
+(setq-default indicate-empty-lines t)
 
-;; Efface automatiquement les espaces a chaque save
+;; Efface automatiquement les espaces de fin de ligne a chaque save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-
 
 ;; Style pour l'export org-mode
 (setq org-odt-styles-file "/home/zobi8225/Dropbox/style.ott")
 
+
+;; THEME
+;; ----------------------------------
 ;; Enlève le welcome popup
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -126,61 +195,54 @@
    (quote
     ("2affb26fb9a1b9325f05f4233d08ccbba7ec6e0c99c64681895219f964aac7af" "91faf348ce7c8aa9ec8e2b3885394263da98ace3defb23f07e0ba0a76d427d46" default)))
  '(delete-trailing-lines t)
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(send-mail-function (quote smtpmail-send-it)))
 
 ;; Pas écrire dans le prompt du mini buffer
 (setq minibuffer-prompt-properties (quote (read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)))
 
-;; Activer le monde de développement pour python
-(package-initialize)
-(elpy-enable)
-
-;; Pour Google facilement une connerie
+;; Pour Googlé facilement une connerie
 (defun google ()
- "Google the selected region if any, display a query prompt otherwise."
- (interactive)
- (browse-url
-  (concat
-   "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
-   (url-hexify-string (if mark-active
-			  (buffer-substring (region-beginning) (region-end))
-			  (read-string "Search Google: "))))))
+  "Google the selected region if any, display a query prompt otherwise."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string (if mark-active
+			   (buffer-substring (region-beginning) (region-end))
+			 (read-string "Search Google: "))))))
 (global-set-key (kbd "C-x g") 'google)
 
 
 
 ;;; Youtube : (useless)
 (defun youtube ()
-"Search YouTube with a query or region if any."
-(interactive)
-(browse-url
- (concat
-  "http://www.youtube.com/results?search_query="
-  (url-hexify-string (if mark-active
-			 (buffer-substring (region-beginning) (region-end))
+  "Search YouTube with a query or region if any."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.youtube.com/results?search_query="
+    (url-hexify-string (if mark-active
+			   (buffer-substring (region-beginning) (region-end))
                          (read-string "Search YouTube: "))))))
 (global-set-key (kbd "C-x y") 'youtube)
 
-;;; Augmente ou diminue rapidement la taille des fenetre (C-X 2/3)
-
+;; Augmente ou diminue rapidement la taille des fenetre (C-X 2/3)
+;; Ne marche pas dans emacs -nw :-(
 (global-set-key (kbd "C-s-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "C-s-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "C-s-<down>") 'shrink-window)
 (global-set-key (kbd "C-s-<up>") 'enlarge-window)
 
-;;; Python Insert UTF8 dans les début de fichier
-(defun python-insert-utf8()
-  "Insert default uft-8 encoding for python"
-  (interactive)
-  (newline)
-  (goto-char (point-min))
-  (insert "# -*- coding: utf-8 -*-")
-  (next-line))
 
-(add-hook 'python-mode-hook 'python-insert-utf8)
+;; UTF8 Partout : Parce que c'est le turfu
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 
 
 ;; Terminal at your fingerprint
+;; -----------------------------------------------------------------
 (defun visit-term-buffer ()
   "Create or visit a terminal buffer."
   (interactive)
@@ -199,24 +261,14 @@
 	    (delete* "+" org-emphasis-alist :key 'car :test 'equal)))
 
 
-;; Test IDO -> Pas sûr que ca fonctionne
-(require 'ido)
-	(ido-mode t)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; EMOJI :smile:
+;; EMOJI -> Ne marche pas dans le emacs -nw , mais marche bien sinon :smile:
 (add-hook 'after-init-hook #'global-emojify-mode)
 
-;; - EMAIL -
-
+;; EMAIL : MU4E
+;;-------------------------------------------------------
 (require 'mu4e)
 
-;; default
+;; Default
 (setq mu4e-maildir (expand-file-name "~/Maildir"))
 
 (setq mu4e-drafts-folder "/[Gmail].Drafts")
@@ -240,15 +292,21 @@
 (setq
  user-mail-address "zobi8225@gmail.com"
  user-full-name  "Thomas Luquet"
- ;; message-signature
- ;;  (concat
- ;;    "Foo X. Bar\n"
- ;;    "http://www.example.com\n")
-)
+ message-signature
+ (concat
+  "-- \n"
+  "Thomas Luquet\n")
+ )
+
+
+;; Permet (théoriquement) d'envoyer des mails avec org-mode
+(require 'org-mu4e)
+
 
 ;; sending mail -- replace USERNAME with your gmail username
 ;; also, make sure the gnutls command line utils are installed
 ;; package 'gnutls-bin' in Debian/Ubuntu, 'gnutls' in Archlinux.
+
 
 (require 'smtpmail)
 
@@ -276,8 +334,20 @@
 (require 'mu4e-maildirs-extension)
 (mu4e-maildirs-extension)
 
-;; Open URL
+;; Open URL dans emacs
+;; ------------------------------------------------------------
 ;; Use firefox to open urls
 (setq browse-url-browser-function 'browse-url-firefox)
 ;; Use qutebrowser to open urls
 (setq browse-url-generic-program "qupzilla")
+
+(global-set-key (kbd "C-c C-o") 'link-hint-open-link-at-point)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+
+;; Python : ELPY
+;; ------------------------------------------------------------
+;; Permet d'afficher la norme dans python
+(package-initialize)
+(elpy-enable)
