1921f9abd2542a56fb6481e363b4eddd39bdf966
Thoams Luquet <thomas@luquet.net>
2016-12-27 17:26:05 +0100

    big up

--- init.el
+++ init.el
@@ -33,17 +33,6 @@
 ;; La on dit que c'est pour tout
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; Company pour emoji :-)
-(require 'company-emoji)
-(add-to-list 'company-backends 'company-emoji)
-;;(emoji-fontset-enable "Symbola") ;; Permet d'utiliser symbola
-
-
-
-;; Company pour le HTML
-;; Jamais testé
-;; (require 'company-web)
-
 
 ;; Don't enable company-mode in below major modes : pas dans le shell, ni erc ...
 (setq company-global-modes '(not eshell-mode comint-mode erc-mode rcirc-mode))
@@ -57,11 +46,6 @@
 
   ;; company-ispell is the plugin to complete words
   (add-to-list 'company-backends 'company-ispell) )
-
-  ;; => Désactivé
-  ;; OPTIONAL, if `company-ispell-dictionary' is nil, `ispell-complete-word-dict' is used
-  ;;  but I prefer hard code the dictionary path. That's more portable.
-  ;; (setq company-ispell-dictionary (file-truename "~/.emacs.d/misc/english-words.txt")))
 
 (add-hook 'text-mode-hook 'text-mode-hook-setup)
 
@@ -205,8 +189,7 @@
     ("7997e0765add4bfcdecb5ac3ee7f64bbb03018fb1ac5597c64ccca8c88b1262f" "4904daa168519536b08ca4655d798ca0fb50d3545e6244cefcf7d0c7b338af7e" "2affb26fb9a1b9325f05f4233d08ccbba7ec6e0c99c64681895219f964aac7af" "91faf348ce7c8aa9ec8e2b3885394263da98ace3defb23f07e0ba0a76d427d46" default)))
  '(delete-trailing-lines t)
  '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/myCloud/agenda/agenda.org")))
- '(send-mail-function (quote smtpmail-send-it)))
+ )
 
 ;; Pas écrire dans le prompt du mini buffer
 (setq minibuffer-prompt-properties (quote (read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)))
@@ -253,7 +236,7 @@
 
 ;; Terminal at your fingerprint
 ;; -----------------------------------------------------------------
-;; Config pour avoir un termina (ansi-term) qui sot bien
+;; Config pour avoir un terminal (ansi-term) qui soit bien
 (defun visit-term-buffer ()
   "Create or visit a terminal buffer."
   (interactive)
@@ -266,142 +249,12 @@
 (global-set-key (kbd "C-c t") 'visit-term-buffer)
 
 
-
-;; EMOJI -> Ne marche pas dans le emacs -nw , mais marche bien sinon :smile:
-(add-hook 'after-init-hook #'global-emojify-mode)
-
-;; ;; EMAIL : MU4E
-;; ;;-------------------------------------------------------
-;; (require 'mu4e)
-
-;; ;; Default
-;; (setq mu4e-maildir (expand-file-name "~/Maildir"))
-
-;; (setq mu4e-drafts-folder "/[Gmail].Drafts")
-;; (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
-;; (setq mu4e-trash-folder  "/[Gmail].Trash")
-
-;; ;; don't save message to Sent Messages, GMail/IMAP will take care of this
-;; (setq mu4e-sent-messages-behavior 'delete)
-
-;; ;; setup some handy shortcuts
-;; (setq mu4e-maildir-shortcuts
-;;       '(("/INBOX"             . ?i)
-;;         ("/[Gmail].Sent Mail" . ?s)
-;;         ("/[Gmail].Trash"     . ?t)))
-
-;; ;; allow for updating mail using 'U' in the main view:
-;; (setq mu4e-get-mail-command "offlineimap")
-
-;; ;; Dossier des fichiers
-;; (setq mu4e-attachment-dir "~/Desktop/mailDl/")
-
-
-;; ;; something about ourselves
-;; ;; I don't use a signature...
-;; (setq
-;;  user-mail-address "zobi8225@gmail.com"
-;;  user-full-name  "Thomas Luquet"
-;;  message-signature
-;;  (concat
-;;   "-- \n"
-;;   "Thomas Luquet\n"
-;;   "thomas@luquet.net\n")
-;;  )
-
-
-;; ;; Permet (théoriquement) d'envoyer des mails avec org-mode
-;; (require 'org-mu4e)
-
-
-;; ;; sending mail -- replace USERNAME with your gmail username
-;; ;; also, make sure the gnutls command line utils are installed
-;; ;; package 'gnutls-bin' in Debian/Ubuntu, 'gnutls' in Archlinux.
-
-
-;; (require 'smtpmail)
-
-;; (setq message-send-mail-function 'smtpmail-send-it
-;;       starttls-use-gnutls t
-;;       smtpmail-starttls-credentials
-;;       '(("smtp.gmail.com" 587 nil nil))
-;;       smtpmail-auth-credentials
-;;       (expand-file-name "~/.authinfo.gpg")
-;;       smtpmail-default-smtp-server "smtp.gmail.com"
-;;       smtpmail-smtp-server "smtp.gmail.com"
-;;       smtpmail-smtp-service 587
-;;       smtpmail-debug-info t)
-
-;; ;; View
-;; (require 'mu4e-contrib)
-;; (setq mu4e-html2text-command 'mu4e-shr2text)
-
-
-;; ;; NOTIFICATION
-;; (mu4e-alert-set-default-style 'libnotify)
-;; (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
-;; (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display) ;; Dans la ligne
-
-;; ;; Mail dir extention
-;; (require 'mu4e-maildirs-extension)
-;; (mu4e-maildirs-extension)
-
-;; ;; use 'fancy' non-ascii characters in various places in mu4e
-;; (setq mu4e-use-fancy-chars t)
-
-
-;; ;; A CHECKER don't save message to Sent Messages, Gmail/IMAP takes care of this
-;; ;; (setq mu4e-sent-messages-behavior 'delete)
-
-;; ;; Montrer les images dans emacs
-;; (setq
-;;  mu4e-view-show-images t
-;;  mu4e-view-image-max-width 800)
-
-
-;; (setq mu4e-update-interval 1200) ;; Reload tout les 20 min
-
 ;; Open URL dans emacs
 ;; ------------------------------------------------------------
 ;; Use firefox to open urls
 (setq browse-url-browser-function 'browse-url-firefox)
 ;; Use qutebrowser to open urls
 ;; (setq browse-url-generic-program "qupzilla")
-
-;;(global-set-key (kbd "C-c C-o") 'link-hint-open-link-at-point)
-;;(put 'upcase-region 'disabled nil)
-;;(put 'downcase-region 'disabled nil)
-
-
-;; SMEX -- TEST ---
-;; ------------------------------------------------------------
-;; A pas compris l'utilité
-;; Est sensé retenir les fonctions les plus utilisé dans emacs
-(require 'smex) ; Not needed if you use package.el
-(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
-                  ; when Smex is auto-initialized on its first run.
-
-
-
-;; LANG TOOLS
-;; ------------------------------------------------------------
-;; Permet une correction de la grammaire francaise
-(require 'langtool)
-(setq langtool-java-classpath
-      "/usr/share/languagetool:/usr/share/java/languagetool/*")
-(setq langtool-mother-tongue "fr")
-(setq langtool-default-language "fr")
-
-;; Censé faire un popup (Mais à pas l'aire de marcher...)
-(defun langtool-autoshow-detail-popup (overlays)
-  (when (require 'popup nil t)
-    ;; Do not interrupt current popup
-    (unless (or popup-instances
-                ;; suppress popup after type `C-g` .
-                (memq last-command '(keyboard-quit)))
-      (let ((msg (langtool-details-error-message overlays)))
-        (popup-tip msg)))))
-
 
 
 ;; RAINBOW MODE
@@ -416,7 +269,7 @@
 (require 'helm-config)
 (helm-mode 1)
 
-;;(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ;; Bizarie censer faire que le tab équivaut à un enter
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ;; Bizarie censer faire que le tab équivaut à un enter
 
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -429,15 +282,6 @@
 (global-set-key (kbd "M-s") 'helm-spotify)
 
 
-;; Pour les emails
-;; (require 'helm-mu) ;; Pour les email
-;; (autoload 'helm-mu "helm-mu" "" t) ;; Pas trop compris
-;; (autoload 'helm-mu-contacts "helm-mu" "" t) ;; pas trop compris
-;; Pour flycheck
-;;(require 'helm-flycheck) ;; Not necessary if using ELPA package
-;;(eval-after-load 'flycheck
-;;  '(define-key flycheck-mode-map (kbd "C-c ! h") 'helm-flycheck))
-
 ;; Pour company
 (eval-after-load 'company
   '(progn
@@ -449,35 +293,5 @@
 ;; Pour flyspell -> Check si ca marche vraiment
 (define-key flyspell-mode-map (kbd "C-;") 'helm-flyspell-correct)
 
-;; Projectitle (Pas utile pour moi je pense)
-;; MIT EN TEST
-;; (projectile-global-mode)
-;; (setq projectile-indexing-method 'native)
-;; (setq projectile-enable-caching t)
-;; (setq projectile-completion-system 'helm)
-;; (helm-projectile-on)
 
 
-
-
-;; ;; Twittering Mode
-;; ;; ------------------------------------------------------------
-;; (require 'twittering-mode)
-;; (setq twittering-use-master-password t)
-;; (setq twittering-icon-mode t)
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
-
-
-;; TEST du stop debug (pour pelican) ?
-(setq ad-redefinition-action 'accept)
-
-(setq org-capture-templates
-      (quote (
-	      ("x" "org-protocol" entry (file "~/web.org")
-	       "* TODO Review %c\n%U\n%i\n" :immediate-finish))))
-(put 'erase-buffer 'disabled nil)
