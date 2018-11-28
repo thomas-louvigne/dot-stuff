ff009c3ebafd9c117cfb21c165a5d37a7e755602
Thomas Luquet <thomas@luquet.net>
2018-11-28 14:26:58 +0100

    update manythings in my dot emacs

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;;; package --- My DOT-file
+;;; Commentary:
+;; It is my EMACS conf, enjoy it
+;;; Code:
 (package-initialize)
 
 (require 'use-package)
@@ -15,14 +19,12 @@
 (setq-default visible-bell 0
               indent-tabs-mode nil)
 
-;; Keeps me from fat fingering C-x b
-;;(global-set-key (kbd "C-x C-b") 'counsel-switch-to-buffer-or-window)
-
 ;; Kill all line
 (global-set-key (kbd "C-d") 'kill-whole-line) ;; fait un couper de toute la ligne, remplace du kill-char totalement uselss
 
-;; Disable autosave because it's slow
-(auto-save-mode -1)
+;; Anable autosave
+(setq auto-save-default nil)
+(savehist-mode t)
 
 ;; Auto-revert
 (global-auto-revert-mode t)
@@ -39,9 +41,11 @@
 ;; Expand region
 (use-package expand-region
   :bind ("C-c =" . er/expand-region)
+  :bind ("M-d" . er/expand-region)
   )
 
-;; A mettre sur W:bind ("M-S-w" . er/expand-region)
+;; Mouse trick to change windows focus or resizing with click
+(xterm-mouse-mode t)
 
 ;; Switch windows
 (global-set-key (kbd "<f6>") #'other-window)
@@ -58,6 +62,7 @@
           )
   :bind
   ("M-x" . helm-M-x)
+  ("M-q" . helm-imenu)
   ("C-x C-f" . #'helm-find-files)
   ("C-x C-b" . #'helm-buffers-list)
   ("C-s" . #'swiper-helm)
@@ -70,12 +75,18 @@
 (use-package helm-projectile)
 
 ;; Manage projects with a keystroke
+
+(setq projectile-enable-caching t)
+(setq projectile-indexing-method 'native)
+(setq projectile-ignored-directories '("_output" "node_module" "pkg"))
+
 (use-package projectile
   :init (progn
           (setq projectile-enable-caching t)
           (setq projectile-indexing-method 'native)
+          (setq projectile-globally-ignored-directories '("node_module"))
           (setq projectile-ignored-directories '("_output" "node_module" "pkg"))
-          (setq projectile-ignored-files '(".DS_Store" ".gitmodules" ".gitignore") )
+          (setq projectile-ignored-files '(".DS_Store" ".gitmodules" ".gitignore" "package-lock.json") )
           )
   :bind (
          ("<f1>" . helm-projectile-switch-project) ;; Change le projet de travail
@@ -84,6 +95,11 @@
          )
   :config (projectile-mode 1))
 
+
+
+;; Let helm-ag searching at point
+(setq helm-ag-insert-at-point 'symbol)
+
 ;; If nothing is marked yanks whole line
 (use-package whole-line-or-region
   :config (whole-line-or-region-global-mode 1))
@@ -91,15 +107,6 @@
 ;; Autocomplete Popups
 (use-package company
   :config (global-company-mode 1))
-
-;; Gitgutter
-(use-package diff-hl
-  :hook (magit-post-refresh . diff-hl-magit-post-refresh)
-  :bind(
-        ("C-n" . git-gutter:next-hunk)
-        ("C-p" . git-gutter:previous-hunk)
-        )
-  :config (global-diff-hl-mode 1))
 
 ;; Yaml editing support
 (use-package yaml-mode
@@ -144,20 +151,10 @@
                 (or (buffer-file-name) default-directory)
                 "node_modules"))
          (eslint (and root
-                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                      (expand-file-name "node_modules/.bin/eslint"
                                         root))))
     (when (and eslint (file-executable-p eslint))
       (setq-local flycheck-javascript-eslint-executable eslint))))
-
-(defun my/use-tslint-from-node-modules ()
-  (let* ((root (locate-dominating-file
-                (or (buffer-file-name) default-directory)
-                "node_modules"))
-         (tslint (and root
-                      (expand-file-name "node_modules/tslint/bin/tslint"
-                                        root))))
-    (when (and tslint (file-executable-p tslint))
-      (setq-local flycheck-typescript-tslint-executable tslint))))
 
 ;; Le package n'étant pas terrible j'ai pris ca ici :
 ;; https://github.com/rustyconover/eslint-fix/blob/483709dbad2100160df770a87c35e00248bb8f68/eslint-fix.el
@@ -175,14 +172,24 @@
     ))
 
 
+;; (use-package flycheck
+;;   :hook ((flycheck-mode . my/use-tslint-from-node-modules)
+;;          (flycheck-mode . my/use-eslint-from-node-modules))
+;;   :config
+;;   (global-flycheck-mode)
+;;   (setq-default flycheck-disabled-checkers '(javascript-jshint))
+;;   (flycheck-add-mode 'javascript-eslint 'js2-mode)
+;;   (flycheck-add-mode 'typescript-tslint 'typescript-mode))
+
+;; (global-flycheck-mode)
 (use-package flycheck
-  :hook ((flycheck-mode . my/use-tslint-from-node-modules)
-         (flycheck-mode . my/use-eslint-from-node-modules))
   :config
-  (global-flycheck-mode)
+
   (setq-default flycheck-disabled-checkers '(javascript-jshint))
   (flycheck-add-mode 'javascript-eslint 'js2-mode)
   (flycheck-add-mode 'typescript-tslint 'typescript-mode))
+
+;; (setq max-lisp-eval-depth 10000)
 
 ;; Maybe I can finally start using it (maybe)
 (use-package ace-jump-mode
@@ -208,14 +215,17 @@
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
-
 (add-hook 'prog-mode-hook 'show-paren-mode)
-(setq linum-format "%2d|") ;; ajout un pip de séparation
-(add-hook 'prog-mode-hook 'linum-mode)
+
+
+;; Try other stuff
+(global-display-line-numbers-mode)
+
 (setq-default cursor-type 'box)
-(set-frame-font "Roboto Mono 12")
+(set-frame-font "Roboto Mono 11")
 
 ;; Disable backup files (# and ~ files)
+;; http://ergoemacs.org/emacs/emacs_set_backup_into_a_directory.html
 (setq make-backup-files nil
       auto-save-default nil)
 
@@ -231,7 +241,6 @@
 ;; Smart C-a
 (defun smart-beginning-of-line ()
   "Move point to first non-whitespace character or beginning-of-line.
-
 Move point to the first non-whitespace character on this line.
 If point was already at that position, move point to beginning of line."
   (interactive)
@@ -285,10 +294,15 @@ If point was already at that position, move point to beginning of line."
 (diminish 'edebug-mode)
 (diminish 'flycheck-mode)
 (diminish 'helm-mode)
+(diminish 'helm)
 (diminish 'rainbow-delimiters-mode)
 (diminish 'python-mode)
 (diminish 'eldoc-mode)
 (diminish 'git-gutter)
+(diminish 'git-gutter+-mode)
+(diminish 'git-rebase-mode)
+(diminish 'git-gutter-mode-major-mode)
+(diminish 'magit-mode)
 (diminish 'python)
 (diminish 'elpy-mode)
 (diminish 'projectile-mode)
@@ -299,16 +313,137 @@ If point was already at that position, move point to beginning of line."
 (diminish 'js2-mode)
 (diminish 'company-mode)
 (diminish 'ivy-mode)
-
+(diminish 'undo-tree-mode)
+(diminish 'lisp-mode)
+(diminish 'editorconfig-mode)
+(diminish 'whole-line-or-region-mode)
+(diminish 'helm-modes-using-escaped-strings)
+(diminish 'compilation-mode)
+(diminish 'emacs-lisp-mode)
 
 ;; [TEST] Editor Config
 (use-package editorconfig
   :ensure t
   :config
   (editorconfig-mode 1))
-;; Hugo blog
 
+;; Hugo blog
 (use-package ox-hugo
   :after ox)
 
-;;; init.el ends here
+;; [TEST] Go to variable OR function
+(defun bzg-find-variable-or-function-at-point ()
+  (interactive)
+  (or (find-variable-at-point)
+      (find-function-at-point)
+      (message "No variable or function at point.")))
+
+(global-set-key (kbd "C-.") 'bzg-find-variable-or-function-at-point)
+
+;; Dumb-jump
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-RET" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'helm) ;; (setq dumb-jump-selector 'helm)
+  :ensure)
+
+;; dired-sidebar
+(use-package dired-sidebar
+  :bind (("C-x C-n" . sidebar-toggle))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-theme 'doom-themes) ;; j'ai pas l'impression que ca marche
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
+
+(defun sidebar-toggle ()
+  "Toggle both `dired-sidebar' and `ibuffer-sidebar'."
+  (interactive)
+  (dired-sidebar-toggle-sidebar)
+  (ibuffer-sidebar-toggle-sidebar))
+
+;; Grammalecte - French grammar
+;; (require 'flycheck-grammalecte)
+;; (load-file "~/.emacs.d/flycheck-grammalecte/flycheck-grammalecte.elc")
+;; (setq flycheck-grammalecte-report-apos nil
+;;       flycheck-grammalecte-report-nbsp nil)
+;; (setq flycheck-grammalecte-enabled-modes
+;;       '(org-mode text-mode markdown-mode))
+
+;; Open the file name being pointed in an other window or dired
+;; reference: http://kouzuka.blogspot.com/2011/02/emacsurlfinder.html
+(defun my-directory-or-file-p (path)
+  "Return t if PATH is a directory, return nil if PATH is a file."
+  (car (file-attributes path)))
+
+;; (defun my-open-emacs-at-point ()
+;;   "Open the file with opening EMACS."
+;;   (interactive)
+;;   (require 'ffap)
+;;   (let ((file (or (ffap-url-at-point)
+;;                   (ffap-file-at-point))))
+;;     (unless (stringp file)
+;;       (error"No file or URL found"))
+;;     (when (file-exists-p (expand-file-name file))
+;;       (setq file (expand-file-name file)))
+;;     (message "Open: %s" file)
+
+;;     (if (my-directory-or-file-p file)
+;;       (dired-other-window file)
+;;       (find-file-other-window file))
+;;     ))
+
+;; (global-set-key (kbd "\C-c o") 'my-open-emacs-at-point)
+
+;; double click
+(global-set-key [double-mouse-1] 'my-open-emacs-at-point)
+(global-set-key [double-down-mouse-1] 'ignore) ; mouse-drag-region
+
+;; TODO
+;;--------
+;; (defun find-file-or-jump-to-definition ()
+;;   "Find a file or jump to a definition."
+;;   (interactive)
+;;   (let* ((tap (thing-at-point 'filename))
+;;          (fname (when tap (file-relative-name tap default-directory))))
+;;     (if (and (fname (file-exists-p fname)))
+;;         (find-file fname)
+;;       (js2-jump-to-definition))))
+
+;; Switch to camelCase
+(require 'string-inflection)
+(global-set-key (kbd "C-c c") 'string-inflection-lower-camelcase)
+
+
+;; Le you know what you have modified during this commit
+(require 'git-gutter)
+
+;; If you enable global minor mode
+(global-git-gutter-mode t)
+(global-set-key (kbd "C-x C-g") 'git-gutter)
+(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+;; Jump to next/previous hunk
+(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+;; Stage current hunk
+(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+;; Revert current hunk
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+;; Mark current hunk
+(global-set-key (kbd "C-x v SPC") #'git-gutter:mark-hunk)
+
+
+;;; Init.el ends here
