b13e7ee5f698d00ca86afc283998afe25d6dc017
Thomas Luquet <thomas@luquet.net>
2023-04-14 13:18:04 +0200

    update emacs conf file

--- init-locales.el
+++ init-locales.el
@@ -2,6 +2,39 @@
 ;;; Commentary:
 ;;; Code:
 
+;; MY CODE
+
+
+;; Use-package
+(require 'use-package)
+
+;; Expand region
+(use-package expand-region
+  :bind ("M-s" . er/expand-region)
+  :bind ("M-S-s" . er/contract-region)
+  )
+
+;; Stop trailing whitespace
+(setq-default show-trailing-whitespace t)
+(setq-default show-leading-whitespace t)
+(setq-default indicate-empty-lines t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+
+(use-package easy-hugo
+  :init
+  (setq easy-hugo-default-ext ".org")
+  (setq easy-hugo-org-header t)
+  (setq easy-hugo-basedir "~/Documents/blogs/blog-louvigne/")
+  (setq easy-hugo-url "http://thomas-louvigne.github.io/")
+  (setq easy-hugo-sshdomain "blogdomain")
+  (setq easy-hugo-root "~/Documents/blogs/blog-louvigne/public/")
+  (setq easy-hugo-previewtime "300")
+  (setq easy-hugo-postdir "content/posts")
+  :bind ("C-c C-e" . easy-hugo))
+
+
+;; END OF MY CODE
 (defun sanityinc/locale-var-encoding (v)
   "Return the encoding portion of the locale string V, or nil if missing."
   (when v
@@ -25,8 +58,6 @@
 (provide 'init-locales)
 
 
-(require 'expand-region)
-(global-set-key (kbd "meta-d") 'er/expand-region)
 
 
 
