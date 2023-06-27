3deb06924e59864b1f4555b8f6601034e28018f5
Thomas Babord <thomas@luquet.net>
2023-06-27 14:16:17 +0200

    update emacs conf

--- init-locales.el
+++ init-locales.el
@@ -4,9 +4,6 @@
 
 ;; MY CODE
 
-
-(require 'use-package)
-
 ;; Expand region
 (use-package expand-region
   :bind ("M-s" . er/expand-region)
@@ -34,42 +31,28 @@
   :bind ("C-c C-e" . easy-hugo))
 
 ;; Correcteur gramaticale
+
+;; Installation :
 (with-eval-after-load 'flycheck
   (flycheck-grammalecte-setup))
+;; Finally, the last expected step is to download the
+;; CLI & Server upstream package. Just enter the following command:
+;; M-x grammalecte-download-grammalecte
+
+
 (use-package flycheck-grammalecte
   :hook (fountain-mode . flycheck-mode)
   :init
   (setq flycheck-grammalecte-report-apos nil
-        flycheck-grammalecte-report-esp nil
+        flycheck-grammalecte-report-esp t
         flycheck-grammalecte-report-nbsp nil)
   :config
   (add-to-list 'flycheck-grammalecte-enabled-modes 'fountain-mode)
   (grammalecte-download-grammalecte)
   (flycheck-grammalecte-setup))
 
-;; Bette org mode
+;; Better org mode
 (global-org-bulletproof-mode +1)
-
-;; END OF MY CODE
-(defun sanityinc/locale-var-encoding (v)
-  "Return the encoding portion of the locale string V, or nil if missing."
-  (when v
-    (save-match-data
-      (let ((case-fold-search t))
-        (when (string-match "\\.\\([^.]*\\)\\'" v)
-          (intern (downcase (match-string 1 v))))))))
-
-(dolist (varname '("LC_ALL" "LANG" "LC_CTYPE"))
-  (let ((encoding (sanityinc/locale-var-encoding (getenv varname))))
-    (unless (memq encoding '(nil utf8 utf-8))
-      (message "Warning: non-UTF8 encoding in environment variable %s may cause interop problems with this Emacs configuration." varname))))
-
-(when (fboundp 'set-charset-priority)
-  (set-charset-priority 'unicode))
-(prefer-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-(unless (eq system-type 'windows-nt)
-  (set-selection-coding-system 'utf-8))
 
 ;; Fin des conf locales
 (provide 'init-locales)
