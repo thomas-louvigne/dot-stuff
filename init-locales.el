d58d0759c77f338ff785d80a07cff36b97bbb367
Thomas Babord <thomas@luquet.net>
2023-08-25 11:57:32 +0200

    update pour fichier text + org pour flycheck grammacalect

--- init-locales.el
+++ init-locales.el
@@ -17,42 +17,47 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 
-;; Pas hyper utile
-(use-package easy-hugo
-  :init
-  (setq easy-hugo-default-ext ".org")
-  (setq easy-hugo-org-header t)
-  (setq easy-hugo-basedir "~/Documents/blogs/blog-louvigne/")
-  (setq easy-hugo-url "http://thomas-louvigne.github.io/")
-  (setq easy-hugo-sshdomain "blogdomain")
-  (setq easy-hugo-root "~/Documents/blogs/blog-louvigne/public/")
-  (setq easy-hugo-previewtime "300")
-  (setq easy-hugo-postdir "content/posts")
-  :bind ("C-c C-e" . easy-hugo))
+;; Force les fichiers .txt a s'ouvre dans le text-mode
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . text-mode))
 
 ;; Correcteur gramaticale
 
-;; Installation :
-(with-eval-after-load 'flycheck
-  (flycheck-grammalecte-setup))
-;; Finally, the last expected step is to download the
-;; CLI & Server upstream package. Just enter the following command:
-;; M-x grammalecte-download-grammalecte
-
+;; Setup :
+;; (with-eval-after-load 'flycheck
+;;   (flycheck-grammalecte-setup))
 
 (use-package flycheck-grammalecte
   :hook (fountain-mode . flycheck-mode)
   :init
   (setq flycheck-grammalecte-report-apos nil
         flycheck-grammalecte-report-esp t
-        flycheck-grammalecte-report-nbsp nil)
+        flycheck-grammalecte-report-nbsp nil
+        ;; Ajout trouvé sur les internets
+        flycheck-grammalecte-filters-by-mode
+        '(
+          (org-mode "(?ims)^[ \t]*#\\+begin_src.+?#\\+end_src"
+                    "(?im)^[ \t]*#\\+begin[_:].+$"
+                    "(?im)^[ \t]*#\\+end[_:].+$"
+                    "(?m)^[ \t]*(?:DEADLINE|SCHEDULED):.+$"
+                    "(?m)^\\*+ .*[ \t]*(:[\\w:@]+:)[ \t]*$"
+                    "(?m)^[ \t]*-[ \t]*" ;; tirets pour orgmode
+                    "(?im)^:[a-z]+:" ;; propriétés org-roam
+                    "(?im)^[ \t]*#\\+(?:caption|description|keywords|(?:sub)?title):"
+                    "(?im)^[ \t]*#\\+(?!caption|description|keywords|(?:sub)?title)\\w+:.*$")
+          (text-mode "(?m)^[ \t]*-[ \t]*" ;; tirets pour textmode
+                     )
+          (message-mode "(?m)^[ \t]*(?:[\\w_.]+>|[]>|]).*"))
+        )
   :config
   (add-to-list 'flycheck-grammalecte-enabled-modes 'fountain-mode)
   (grammalecte-download-grammalecte)
   (flycheck-grammalecte-setup))
 
 ;; Better org mode
-(global-org-bulletproof-mode +1)
+(use-package org-bulletproof
+  :init
+  (setq global-org-bulletproof-mode t)
+  )
 
 ;; Fin des conf locales
 (provide 'init-locales)
